bcfdad5b5cabccbeb1dde04c8544f1609dbd00fb
maamoun <mribahmaamoun@gmail.com>
2023-04-10 00:23:45 +0100

    dockercompose

--- DrinkAndGo/Program.cs
+++ DrinkAndGo/Program.cs
@@ -11,7 +11,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 // DbContext Configuration
-<<<<<<< Updated upstream
 var DbHost = Environment.GetEnvironmentVariable("DB_HOST");
 var DbName = Environment.GetEnvironmentVariable("DB_NAME");
 var connectionString = $"Data Source={DbHost};Initial Catalog={DbName};Integrated Security=True;Pooling=False";
@@ -20,11 +19,6 @@ builder.Services.AddDbContext<AppDbContext>(
     options => 
     options.UseSqlServer(
         builder.Configuration.GetConnectionString(connectionString)
-=======
-builder.Services.AddDbContext<AppDbContext>(
-    options => options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DrinkCS")
->>>>>>> Stashed changes
         ));
 
 // Services Configuration
